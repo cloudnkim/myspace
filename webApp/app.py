d733ac86b3f052ea7af673f11e86bbd8cfdb0b25
cloudnkim <yesksj26@gmail.com>
2020-03-07 00:07:16 +0900

    0.0.0.0 ? why?

--- webApp/app.py
+++ webApp/app.py
@@ -2,12 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello Flask'
+def home():
+	return 'Hello!!'
 
-@app.route('/info')
-def info():
-    return 'Info'
-
-if __name__=='__main__':
- app.run(host="0.0.0.0", port=5000, debug=True)
+app.run(host='0.0.0.0')
